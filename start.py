b1e3d51772b22a583d8ee32ba507db2541ae21b6
Khillo <khalilwarwar2@gmail.com>
2026-01-12 13:52:38 +0200

    add port as optinal

--- start.py
+++ start.py
@@ -4,12 +4,13 @@ import time
 
 path = __file__
 path = path.replace("start.py", "")
+port = sys.argv[1] if (len(sys.argv) >= 2 and sys.argv[1].isnumeric) else "12345"
 
 procceses = []
 print("Starting Internet")
 procceses.append(
     subprocess.Popen(
-        [sys.executable, path + "InternetSim.py", sys.argv[1]],
+        [sys.executable, path + "InternetSim.py", port],
         creationflags=subprocess.CREATE_NEW_CONSOLE,
     )
 )
@@ -17,14 +18,14 @@ time.sleep(1)
 print("Starting Alice")
 procceses.append(
     subprocess.Popen(
-        [sys.executable, path + "ChatSim.py", sys.argv[1], "alice"],
+        [sys.executable, path + "ChatSim.py", port, "alice"],
         creationflags=subprocess.CREATE_NEW_CONSOLE,
     )
 )
 print("Starting Bob")
 procceses.append(
     subprocess.Popen(
-        [sys.executable, path + "ChatSim.py", sys.argv[1], "bob"],
+        [sys.executable, path + "ChatSim.py", port, "bob"],
         creationflags=subprocess.CREATE_NEW_CONSOLE,
     )
 )
